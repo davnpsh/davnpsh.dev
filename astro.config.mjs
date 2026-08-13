7443e481155166206f3974fe7264d7ad5ed0b9ad
davnpsh <me@davnpsh.dev>
2026-08-13 12:50:56 -0500

    add: somewhat the structure I want

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Unifont",
+      cssVariable: "--font-unifont",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Unifont.otf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
